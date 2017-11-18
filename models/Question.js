40f953485b486902e7bb4e66e8be002a710b6547
Bomi <bkim1289@gmail.com>
2017-11-18 00:12:55 +0000

    automatically backed up by learn

--- models/Question.js
+++ models/Question.js
@@ -19,10 +19,11 @@ class Question{
   }
 
   insert(){
-    const sql=`INSERT INTO questions (content) VALUES (?)`
+    const sql = `INSERT INTO questions (content) VALUES (?)`
     return new Promise(function(resolve){
       db.run(sql, [this.content], function(err, result){
-      resolve("Row inserted!")
+        resolve("Row inserted!")
+      })
     })
   }
 
